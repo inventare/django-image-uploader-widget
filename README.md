69eb9b5821ad28fad453b162dd0a6be7ce096e84
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-06-06 13:49:49 -0300

    add files for building python package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Image Uploader Widget for Django
+
+**Warning**: This was developed using the more recent version of django and i have not tested this with older django versions (because i created this for use in some recent projects). Is not my intentions to made this complete compatible with old versions of django, but if you want to try to use this with older versions of django and get some erros, please **contact-me** (or open a **issue** here) and we can talk about the errors and made this compatible to the version that you are geting errors.
+
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='image_uploader_widget',
+    version='0.0.1',
+    description='Simple Image Uploader Widget for Django-Admin',
+    long_description=readme,
+    author='Eduardo Oliveira',
+    author_email='eduardo_y05@outlook.com',
+    url='https://github.com/EduardoJM/django-image-uploader-widget',
+    license='MIT',
+    packages=find_packages(
+        exclude = ('image_uploader_widget_demo',)
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django,admin,widget,image,uploader',
+)
