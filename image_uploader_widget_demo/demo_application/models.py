aad3316bf6f9ab37fc143e533af77ff9cc1f5188
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-06-06 10:23:33 -0300

    update demo

--- image_uploader_widget_demo/demo_application/models.py
+++ image_uploader_widget_demo/demo_application/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 class ExampleModel(models.Model):
     image = models.ImageField('Image', null = True, blank = True)
+
+    def __str__(self):
+        return 'Example Model'
+
+    class Meta:
+        verbose_name = 'Example Model'
+        verbose_name_plural = 'Example Models'
