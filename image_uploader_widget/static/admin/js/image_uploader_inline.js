5d21376c71c47efb5239ed044b745fa1f9d7a4ea
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-12-23 21:52:03 -0300

    remove rewrited code from original javascript

--- image_uploader_widget/static/admin/js/image_uploader_inline.js
+++ image_uploader_widget/static/admin/js/image_uploader_inline.js
@@ -44,69 +44,6 @@
                     var blob = URL.createObjectURL(files[0]);
                     $(this).closest('.inline-related').find('img').attr('src', blob);
                 },
-                /**
-                 * Append a '.inline-related' markup to the '.inline-related'.
-                 * @param {HTMLElement} el The element to append the '.inline-related' inner markup.
-                 * @param {String} url The image url of the item.
-                 */
-                appendItem: function(el, url) {
-                    var delete_icon = '';
-                    var item = $(el).closest('.inline-related');
-                    if (item.data('candelete')) {
-                        delete_icon = '<span class="iuw-delete-icon">X</span>'
-                    }
-                    $(el).append(
-                        '<img src="' + url + '" />' + delete_icon
-                    );
-                    item.off('click');
-                    item.on('click', this.callFileClick);
-                    var fileInput = item.find('input[type=file]');
-                    fileInput.off('change');
-                    fileInput.on('change', this.fileInputChange);
-                },
-                /**
-                 * Add image event handler.
-                 */
-                handleAddImage: function(){
-                    var root = $(this).closest('.iuw-inline-root');
-                    var iuw = root.data('iuw');
-                    if (root.find('input[type=file].temp_file').length == 0) {
-                        root.append('<input type="file" class="temp_file" accept="image/*" style="display: none;" />');
-                        root.find('input[type=file].temp_file').on('change', function(e){
-                            var fileList = $(this).prop('files');
-                            $(this).off('change');
-                            $(this).remove();
-                            var template = root.find('.inline-related.empty-form');
-                            var row = template.clone(true)
-                                .removeClass('empty-form')
-                                .removeClass('last-related')
-                                .attr('data-candelete', true)
-                                .attr("id", root.data('prefix') + "-" + root.data('next'));
-                            $(row).insertBefore($(template));
-                            row.find('input[type=file]').prop('files', fileList);
-                            var blob = URL.createObjectURL(fileList[0]);
-                            iuw.appendItem(row, blob);
-                            iuw.updateEmpty(root);
-                            iuw.updateAllIndexes(root);
-                        });
-                    }
-                    root.find('input[type=file].temp_file').trigger('click');
-                },
-                /**
-                 * Initialize the image uploader inline.
-                 */
-                init: function() {
-                    const that = this;
-                    this.elements.each(function(index, element){
-
-                        $(element).find().each(function(index, related){
-                            that.adjustInlineRelated(related);
-                        });
-                        $(element).find('.iuw-add-image-btn').on('click', that.handleAddImage);
-                        $(element).find('.iuw-empty').on('click', that.handleAddImage);
-                        $(element).data('iuw', that);
-                    });
-                }
             };
             handler.init();
             return handler;
