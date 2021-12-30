981725056c8a5a5a3b1d4c0e2051090b64390075
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-12-30 20:38:34 -0300

    begin implementation of the modal showing feature

--- src/ImageUploaderWidget.ts
+++ src/ImageUploaderWidget.ts
@@ -6,6 +6,7 @@ class ImageUploaderWidget {
     dropLabel: HTMLElement;
     canDelete: boolean = false;
     dragging: boolean = false;
+    canPreview: boolean = true;
 
     id: string;
 
@@ -77,10 +78,36 @@ class ImageUploaderWidget {
         this.element.classList.remove('drop-zone');
     }
 
+    onModalClick = (e: Event) => {
+        console.log(e);
+    }
+
+    getOrCreatePreviewModal = (image: HTMLImageElement) : HTMLElement => {
+        let modal = document.getElementById('iuw-modal-element');
+        if (modal) {
+            const preview = modal.querySelector('.iuw-modal-image-preview');
+            preview.innerHTML = '';
+            preview.appendChild(image);
+            return modal;
+        }
+        modal = document.createElement('div');
+        modal.id = 'iuw-modal-element';
+        modal.classList.add('iuw-modal');
+        modal.style.display = 'none';
+        
+        const preview = document.createElement('div');
+        preview.classList.add('iuw-modal-image-preview');
+        preview.appendChild(image);
+        modal.appendChild(preview);
+        
+        document.body.appendChild(modal);
+        return modal;
+    }
+
     onImagePreviewClick = (e: Event) => {
         if (e && e.target) {
             const targetElement = e.target as HTMLElement;
-            if (e && e.target && targetElement.closest('.iuw-delete-icon')) {
+            if (targetElement.closest('.iuw-delete-icon')) {
                 const element = targetElement.closest('.iuw-image-preview');
                 element.parentElement.removeChild(element);
                 this.checkboxInput.checked = true;
@@ -89,6 +116,16 @@ class ImageUploaderWidget {
                 this.raw = null;
                 this.renderWidget();
                 return;
+            }
+            if (targetElement.closest('.iuw-preview-icon')) {
+                const element = targetElement.closest('.iuw-image-preview');
+                let image = element.querySelector('img');
+                if (image) {
+                    image = image.cloneNode(true) as HTMLImageElement;
+                    const modal = this.getOrCreatePreviewModal(image);
+                    modal.style.display = 'block';
+                    return;
+                }
             }
         }
         this.fileInput.click();
@@ -113,6 +150,17 @@ class ImageUploaderWidget {
             span.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="100%" height="100%"><path xmlns="http://www.w3.org/2000/svg" d="m289.94 256 95-95A24 24 0 0 0 351 127l-95 95-95-95a24 24 0 0 0-34 34l95 95-95 95a24 24 0 1 0 34 34l95-95 95 95a24 24 0 0 0 34-34z"></path></svg>';
             preview.appendChild(span);
         }
+
+        if (this.canPreview) {
+            const span = document.createElement('span');
+            span.classList.add('iuw-preview-icon');
+            if (!this.canDelete) {
+                span.classList.add('iuw-only-preview');
+            }
+            span.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="100%" height="100%"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"></path><path xmlns="http://www.w3.org/2000/svg" d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"></path><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"></path></svg>';
+            preview.appendChild(span);
+        }
+
         return preview;
     }
 
