2f19daa3cb653876841c390c04d33cbbfec6f97e
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-12-31 09:38:59 -0300

    add auto field

--- image_uploader_widget_demo/settings.py
+++ image_uploader_widget_demo/settings.py
@@ -90,3 +90,5 @@ STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
