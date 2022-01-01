84db81375b089fe7af7b9c3550635fdf3fcf2606
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-12-31 21:14:28 -0300

    revert django tests for now

--- tests/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from image_uploader_widget.widgets import ImageUploaderWidget
-
-def get_form(required):
-    class ImageOnlyForm(forms.Form):
-        image = forms.ImageField(widget=ImageUploaderWidget(), required = required)
-    return ImageOnlyForm()
