625c72356690b8b54eb2b587d80920ba3b5c5282
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-01-18 08:03:59 -0300

    fix baseUrl empty on docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'django-image-uploader-widget',
   tagline: 'A beautiful image uploader widget for django/django-admin',
   url: 'https://iuw.institutoinventare.com.br/',
-  baseUrl: '',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
