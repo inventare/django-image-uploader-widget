182d41df2586d7d60869711fd903e266082f907a
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-02-18 14:30:13 -0300

    create function with separated code

--- src/Preview/Preview.spec.ts
+++ src/Preview/Preview.spec.ts
@@ -2,11 +2,21 @@ import { renderPreview } from './Preview';
 import { RAW_URL } from '../Widget/__tests__/Utils';
 
 describe('Preview', () => {
+    function renderPreviewAndTest(canDelete: boolean, canPreview: boolean) {
+        // render
+        const element = renderPreview(RAW_URL, canDelete, canPreview);
+        // test
+        expect(element).toHaveClass('iuw-image-preview');
+        expect(element.querySelectorAll('img')).toHaveLength(1);
+        return element;
+    }
+
     test('renderPreview without canDelete and canPreview must render an preview and an img', () => {
-        const element = renderPreview(RAW_URL, false, false);
+        const element = renderPreviewAndTest(false, false);
         
         expect(element).toHaveClass('iuw-image-preview');
         expect(element.querySelectorAll('img')).toHaveLength(1);
+        
         expect(element.querySelectorAll('.iuw-preview-icon')).toHaveLength(0);
         expect(element.querySelectorAll('.iuw-delete-icon')).toHaveLength(0);
         
@@ -16,10 +26,8 @@ describe('Preview', () => {
     });
     
     test('renderPreview with canDelete and without canPreview must render an preview and an img and the delete button', () => {
-        const element = renderPreview(RAW_URL, true, false);
-        
-        expect(element).toHaveClass('iuw-image-preview');
-        expect(element.querySelectorAll('img')).toHaveLength(1);
+        const element = renderPreviewAndTest(true, false);
+
         expect(element.querySelectorAll('.iuw-delete-icon')).toHaveLength(1);
         expect(element.querySelectorAll('.iuw-preview-icon')).toHaveLength(0);
         
@@ -29,10 +37,8 @@ describe('Preview', () => {
     });
     
     test('renderPreview without canDelete and with canPreview must render an preview and an img and the preview button', () => {
-        const element = renderPreview(RAW_URL, false, true);
-        
-        expect(element).toHaveClass('iuw-image-preview');
-        expect(element.querySelectorAll('img')).toHaveLength(1);
+        const element = renderPreviewAndTest(false, true);
+
         expect(element.querySelectorAll('.iuw-delete-icon')).toHaveLength(0);
         expect(element.querySelectorAll('.iuw-preview-icon')).toHaveLength(1);
         
@@ -42,10 +48,8 @@ describe('Preview', () => {
     });
 
     test('renderPreview with canDelete and canPreview must render an preview and an img and the delete and the preview button', () => {
-        const element = renderPreview(RAW_URL, true, true);
-        
-        expect(element).toHaveClass('iuw-image-preview');
-        expect(element.querySelectorAll('img')).toHaveLength(1);
+        const element = renderPreviewAndTest(true, true);
+
         expect(element.querySelectorAll('.iuw-delete-icon')).toHaveLength(1);
         expect(element.querySelectorAll('.iuw-preview-icon')).toHaveLength(1);
         
