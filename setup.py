0f629a01297e2bf6b23e66d7a7efaa645008930b
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-03-31 22:11:17 -0300

    bump version (#42)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ f.close()
 
 setup(
     name='django-image-uploader-widget',
-    version='0.1.4',
+    version='0.1.6',
     description='Simple Image Uploader Widget for Django-Admin',
     long_description=readme,
     long_description_content_type='text/markdown',
