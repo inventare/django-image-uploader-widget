28eb231a0e093ec68359a283b7cb4d796609a959
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-07 19:20:10 -0300

    refactor: remove useless code

--- image_uploader_widget/widgets.py
+++ image_uploader_widget/widgets.py
@@ -4,13 +4,6 @@ from django.forms import widgets
 class ImageUploaderWidget(widgets.ClearableFileInput):
     template_name = 'admin/widgets/image_uploader_widget.html'
 
-    def __init__(self, attrs = None):
-        super(ImageUploaderWidget, self).__init__(attrs=attrs)
-
-    def get_context(self, name, value, attrs):
-        context = super(ImageUploaderWidget, self).get_context(name, value, attrs)
-        return context
-
     @property
     def media(self):
         return forms.Media(
