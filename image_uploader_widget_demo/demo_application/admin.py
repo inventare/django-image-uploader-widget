35496bd6f70c77e101c1b7ad275c00e41520de2e
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-07 21:24:37 -0300

    feat: begin implementation of demo project for tests

--- image_uploader_widget_demo/demo_application/admin.py
+++ image_uploader_widget_demo/demo_application/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-
 from . import models
 from . import forms
 
 @admin.register(models.TestNonRequired)
 class TestNonRequiredAdmin(admin.ModelAdmin):
     form = forms.TestNonRequiredForm
+
+
+class TestNonRequiredInlineItemAdmin(admin.StackedInline):
+    model = models.TestNonRequiredInlineItem
+    form = forms.TestNonRequiredForm
+    extra = 0
+
+@admin.register(models.TestNonRequiredInline)
+class TestNonRequiredInlineAdmin(admin.ModelAdmin):
+    inlines = [TestNonRequiredInlineItemAdmin]
--- image_uploader_widget_demo/demo_application/models.py
+++ image_uploader_widget_demo/demo_application/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class TestNonRequired(models.Model):
-    image = models.ImageField('Image', null = True, blank = True)
+    image = models.ImageField('Image', null=True, blank=True)
 
     def __str__(self):
         return 'Example Model'
 
     class Meta:
         verbose_name = 'Test Non Required'
-        verbose_name_plural = 'Tests Non Required'
+
+class TestNonRequiredInline(models.Model):
+    class Meta:
+        verbose_name = 'Test Non Required Inline'
+
+class TestNonRequiredInlineItem(models.Model):
+    parent = models.ForeignKey(TestNonRequiredInline, on_delete=models.CASCADE)
+    image = models.ImageField('Image', null=True, blank=True)
