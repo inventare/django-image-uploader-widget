a48b08db4400aa0e470ba50df615572b2fcdd6ca
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-09 19:38:04 -0300

    test: add small change on tests

--- image_uploader_widget_demo/tests/required_widget.py
+++ image_uploader_widget_demo/tests/required_widget.py
@@ -88,6 +88,9 @@ class RequiredWidgetTestCase(IUWTestCase):
         self.assertEqual(len(previews), 1)
         self.assertEqual(root.get_attribute("data-raw"), item.image.url)
 
+        empty_marker = form_row.find_element(By.CSS_SELECTOR, '.iuw-empty')
+        self.assertFalse(empty_marker.is_displayed())
+
         preview = previews[0]
         img = preview.find_element(By.TAG_NAME, 'img')
         preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
