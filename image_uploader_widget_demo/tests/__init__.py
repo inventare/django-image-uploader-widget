2b81fae944575d76f6cc9b51fd91025b755ffd20
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-08 21:52:56 -0300

    test: add tests for the widget inside inline tabular

--- image_uploader_widget_demo/tests/__init__.py
+++ image_uploader_widget_demo/tests/__init__.py
@@ -3,3 +3,4 @@ from .non_required_widget_stacked_inline import *
 from .non_required_widget_tabular_inline import *
 from .required_widget import *
 from .required_widget_stacked_inline import *
+from .required_widget_tabular_inline import *
