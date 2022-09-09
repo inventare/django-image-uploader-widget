c119122edbb307362fc56927d010c9c5bcb6681c
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-08 21:48:25 -0300

    test: add tests to stacked inline required widget

--- image_uploader_widget_demo/tests/__init__.py
+++ image_uploader_widget_demo/tests/__init__.py
@@ -2,3 +2,4 @@ from .non_required_widget import *
 from .non_required_widget_stacked_inline import *
 from .non_required_widget_tabular_inline import *
 from .required_widget import *
+from .required_widget_stacked_inline import *
