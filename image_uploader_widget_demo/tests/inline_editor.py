1b0fc58c5e57527ea34003fe8477ba5202da4b93
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-09-09 13:22:18 -0300

    test: begin adding more test cases for inline editor

--- image_uploader_widget_demo/tests/inline_editor.py
+++ image_uploader_widget_demo/tests/inline_editor.py
@@ -15,16 +15,12 @@ class InlineEditorTestCase(IUWTestCase):
         self.assertIsNotNone(empty)
         self.assertTrue(empty.is_displayed())
 
-    """
     def test_click_on_empty_marker(self):
         self.selenium.get(self.get_url(self.admin_add_url))
 
         root = self.selenium.find_element(By.CSS_SELECTOR, '.iuw-inline-root')
         empty = root.find_element(By.CSS_SELECTOR, '.iuw-empty')
 
-        #empty.click()
-        #temp_file = WebDriverWait(self.selenium, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, ".temp_file"))
-        
         injected_javascript = (
             'const callback = arguments[0];'
             'const input = document.querySelector(".temp_file");'
@@ -33,5 +29,6 @@ class InlineEditorTestCase(IUWTestCase):
         )
         self.selenium.execute_async_script(injected_javascript)
 
-        #self.selenium.switch_to.alert.accept()
-    """
+        empty.click()
+
+        self.selenium.switch_to.alert.accept()
