04aa945ce0d4a09c11d15d8a07797db20cbeefa4
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-11-19 22:00:07 -0300

    test: remove headless=False attribute

--- tests/tests_playwright.py
+++ tests/tests_playwright.py
@@ -14,7 +14,7 @@ class MyTests(StaticLiveServerTestCase):
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         super().setUpClass()
         cls.playwright = sync_playwright().start()
-        cls.browser = cls.playwright.chromium.launch(headless=False)
+        cls.browser = cls.playwright.chromium.launch()
 
     @classmethod
     def tearDownClass(cls):
