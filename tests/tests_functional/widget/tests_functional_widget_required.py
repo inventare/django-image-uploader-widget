892e77dd22d3a4eb2a8c26f6e5daf23ec28a5ec3
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-11-21 22:02:35 -0300

    test: writing test with playwright

--- tests/tests_functional/widget/tests_functional_widget_required.py
+++ tests/tests_functional/widget/tests_functional_widget_required.py
@@ -1,214 +1,229 @@
-from django.test.utils import tag
-#from django.contrib.auth import get_user_model
-#from django.core.files import File
-#from selenium.common.exceptions import NoSuchElementException
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.expected_conditions import invisibility_of_element_located
-#from selenium.webdriver.support.wait import WebDriverWait
-#from tests import models
-#from utils.tests import IUWTestCase
-#from unittest import skip
-#
-#User = get_user_model()
-#
-#@tag('functional')
-#@skip()
-#class RequiredWidgetTestCase(IUWTestCase):
-#    @property
-#    def admin_add_url(self):
-#        path = '/testrequired/add/'
-#        return self.get_url_from_path(path)
-#
-#    def get_edit_url(self, id):
-#        return self.get_url_from_path("/testrequired/%s/change/" % id)
-#
-#    def init_item(self):
-#        item = models.TestRequired()
-#        with open(self.image1, 'rb') as f:
-#            item.image.save("image.png", File(f), False)
-#        item.save()
-#        return item
-#    
-#    @tag("rewrited-in-playwright")
-#    def test_empty_marker_click(self):
-#        """
-#        should emit click event for the file input of the widget when
-#        click on the empty marker.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        self.inject_input_file_clicked()
-#        empty_marker = self.get_widget_empty_marker()
-#        self.assertTrue(empty_marker.is_displayed())
-#
-#        empty_marker.click()
-#        self.assertEqual(self.selenium.switch_to.alert.text, "CLICKED")
-#        self.selenium.switch_to.alert.accept()
-#        
-#    @tag("rewrited-in-playwright")
-#    def test_required_file_input(self):
-#        """
-#        should create the preview image when file input value was changed and
-#        save item in database when submit the form.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        form_row = self.get_widget_form_row()
-#
-#        with self.assertRaises(NoSuchElementException):
-#            preview = self.get_widget_preview(form_row)
-#        
-#        file_input = form_row.find_element(By.CSS_SELECTOR, 'input[type=file]')
-#        self.assertEqual(file_input.get_attribute('value'), "")
-#        
-#        file_input.send_keys(self.image1)
-#
-#        self.assertEqual(file_input.get_attribute('value'), "C:\\fakepath\\image.png")
-#
-#        preview = self.get_widget_preview(form_row)
-#        img = preview.find_element(By.TAG_NAME, 'img')
-#        preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
-#        self.assertTrue(preview.is_displayed())
-#        self.assertIsNotNone(img)
-#        self.assertIsNotNone(preview_button)
-#
-#        submit = self.selenium.find_element(By.CSS_SELECTOR, '#testrequired_form [type="submit"]')
-#        submit.click()
-#
-#        alert = WebDriverWait(self.selenium, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, ".messagelist .success"))
-#        self.assertIsNotNone(alert)
-#        itens = models.TestRequired.objects.all()
-#        self.assertEqual(len(itens), 1)
-#        item = itens[0]
-#        self.assertIsNotNone(item.image)
-#
-#    @tag("rewrited-in-playwright")
-#    def test_image_with_database_data(self):
-#        """
-#        should create the preview with the value from the database when got the field initialized
-#        from database value.
-#        """
-#        item = self.init_item()
-#        self.selenium.get(self.get_edit_url(item.id))
-#
-#        root = self.get_widget_root()
-#        preview = self.get_widget_preview(root)
-#
-#        self.assertIsNotNone(preview)
-#        self.assertEqual(root.get_attribute("data-raw"), item.image.url)
-#
-#        empty_marker = root.find_element(By.CSS_SELECTOR, '.iuw-empty')
-#        self.assertFalse(empty_marker.is_displayed())
-#
-#        img = preview.find_element(By.TAG_NAME, 'img')
-#        preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
-#        self.assertTrue(preview.is_displayed())
-#        self.assertIsNotNone(img)
-#        self.assertTrue(item.image.url in img.get_attribute('src'))
-#        self.assertIsNotNone(preview_button)
-#
-#    def test_click_on_the_preview_image(self):
-#        """
-#        should emit the click event for the file input when click on the preview image.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        form_row = self.get_widget_form_row()
-#        with self.assertRaises(NoSuchElementException):
-#            preview = self.get_widget_preview(form_row)
-#            
-#        file_input = form_row.find_element(By.CSS_SELECTOR, 'input[type=file]')
-#        file_input.send_keys(self.image2)
-#        
-#        self.inject_input_file_clicked()
-#
-#        preview = self.get_widget_preview(form_row)
-#        self.assertIsNotNone(preview)
-#        img = preview.find_element(By.TAG_NAME, 'img')
-#        img.click()
-#        
-#        self.assertEqual(self.selenium.switch_to.alert.text, "CLICKED")
-#        self.selenium.switch_to.alert.accept()
-#
-#    def test_click_on_the_preview_button(self):
-#        """
-#        when click on the preview button, should open the preview modal
-#        with an img tag with the same src of the preview item.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        form_row = self.selenium.find_element(By.CSS_SELECTOR, '.form-row.field-image')
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        file_input = form_row.find_element(By.CSS_SELECTOR, 'input[type=file]')
-#        file_input.send_keys(self.image2)
-#        
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        self.assertEqual(len(previews), 1)
-#        preview = previews[0]
-#        preview_img = preview.find_element(By.TAG_NAME, 'img')
-#
-#        preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
-#        preview_button.click()
-#        
-#        preview_modal = WebDriverWait(self.selenium, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#iuw-modal-element.visible"))
-#        self.assertIsNotNone(preview_modal)
-#        self.assertEqual(preview_modal.get_attribute('class'), 'iuw-modal visible')
-#
-#        img = preview_modal.find_element(By.TAG_NAME, 'img')
-#        self.assertIsNotNone(img)
-#        self.assertEqual(img.get_attribute("src"), preview_img.get_attribute("src"))
-#
-#    def test_click_on_the_preview_button_and_image_on_modal(self):
-#        """
-#        when click on the preview button and open the preview modal, click on the image
-#        should not close the image preview modal.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        form_row = self.selenium.find_element(By.CSS_SELECTOR, '.form-row.field-image')
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        file_input = form_row.find_element(By.CSS_SELECTOR, 'input[type=file]')
-#        file_input.send_keys(self.image1)
-#        
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        self.assertEqual(len(previews), 1)
-#        preview = previews[0]
-#
-#        preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
-#        preview_button.click()
-#        
-#        preview_modal = WebDriverWait(self.selenium, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#iuw-modal-element.visible"))
-#
-#        img = preview_modal.find_element(By.TAG_NAME, 'img')
-#        img.click()
-#
-#        self.selenium.implicitly_wait(0.5)
-#
-#        self.assertEqual(preview_modal.get_attribute("class"), "iuw-modal visible")
-#
-#    def test_click_on_the_preview_button_and_close_on_modal(self):
-#        """
-#        when click on the preview button and open the preview modal, click on the close
-#        modal button should close the image preview modal.
-#        """
-#        self.selenium.get(self.admin_add_url)
-#
-#        form_row = self.selenium.find_element(By.CSS_SELECTOR, '.form-row.field-image')
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        file_input = form_row.find_element(By.CSS_SELECTOR, 'input[type=file]')
-#        file_input.send_keys(self.image1)
-#        
-#        previews = form_row.find_elements(By.CSS_SELECTOR, '.iuw-image-preview')
-#        self.assertEqual(len(previews), 1)
-#        preview = previews[0]
-#
-#        preview_button = preview.find_element(By.CSS_SELECTOR, '.iuw-preview-icon')
-#        preview_button.click()
-#        
-#        preview_modal = WebDriverWait(self.selenium, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#iuw-modal-element.visible"))
-#        
-#        close_button = preview_modal.find_element(By.CSS_SELECTOR, '.iuw-modal-close')
-#        close_button.click()
-#
-#        WebDriverWait(self.selenium, timeout=3).until(invisibility_of_element_located((By.CSS_SELECTOR, "#iuw-modal-element")));
-#
+from django.test import tag
+from django.core.files import File
+from tests import models, TestCase
+
+@tag('playwright')
+class WidgetRequiredTests(TestCase):
+    model = 'testrequired'
+
+    def init_item(self):
+        item = models.TestRequired()
+        with open(self.image1, 'rb') as f:
+            item.image.save("image.png", File(f), False)
+        item.save()
+        return item
+
+    def goto_change_page(self):
+        item = self.init_item()
+        super().goto_change_page(item.id)
+        return item
+
+    def test_should_fire_click_on_file_input_when_click_on_empty_marker(self):
+        """
+        Should fire click event on file input when click on the empty marker.
+
+        The test flow is:
+            - Navigate to Widget Add Page
+            - Click on the empty marker
+            - Assert if click event is fired on input file.
+        """
+        self.goto_add_page()
+        self.inject_input_file_clicked()
+        self.page.query_selector('.form-row.field-image .iuw-empty').click()
+        self.assert_input_file_clicked()
+
+    def test_should_create_preview_and_upload_file(self):
+        """
+        Should show create a preview when select file and upload it when submit.
+
+        The test flow is:
+            - Navigate to Widget Add Page.
+            - Assert if none preview is present.
+            - Add file to file input.
+            - Assert if the preview is added to the widget.
+            - Submit the form.
+            - Assert if success message is present.
+            - Validate created instance.
+        """
+        self.goto_add_page()
+
+        form_row = self.page.query_selector('.form-row.field-image')
+        preview = form_row.query_selector('.iuw-image-preview')
+        self.assertEqual(preview, None)
+
+        file_input = form_row.query_selector('input[type=file]')
+        self.assertEqual(file_input.get_attribute('value'), None)
+        file_input.set_input_files(self.image1)
+
+        preview = form_row.query_selector('.iuw-image-preview')
+        img = preview.query_selector('img')
+        preview_button = preview.query_selector('.iuw-preview-icon')
+        self.assertTrue(preview.is_visible())
+        self.assertIsNotNone(img)
+        self.assertIsNotNone(preview_button)
+
+        self.submit_form('#testrequired_form')
+        self.assert_success_message()
+
+        itens = models.TestRequired.objects.all()
+        self.assertEqual(len(itens), 1)
+        item = itens[0]
+        self.assertIsNotNone(item.image)
+
+    def test_should_intiialized_with_preview_when_editing(self):
+        """
+        Should initialized with the preview when open the edit page.
+
+        The test flow is:
+            - Navigate to Widget Change Page.
+            - Assert if preview is found.
+            - Assert the preview data-raw attribute.
+            - Assert if the empty marker is hidden.
+            - Assert if the preview button is visible.
+        """
+        item = self.goto_change_page()
+
+        root = self.find_widget_root()
+        preview = self.find_widget_preview(root)
+
+        self.assertIsNotNone(preview)
+        self.assertEqual(root.get_attribute("data-raw"), item.image.url)
+
+        empty_marker = self.find_empty_marker(root)
+        self.assertFalse(empty_marker.is_visible())
+        
+        img = preview.query_selector('img')
+        preview_button = self.find_preview_icon(preview)
+        self.assertTrue(preview.is_visible())
+        self.assertIsNotNone(img)
+        self.assertTrue(item.image.url in img.get_attribute('src'))
+        self.assertIsNotNone(preview_button)
+
+    def test_should_fire_click_on_file_input_when_click_on_the_preview_image(self):
+        """
+        Should fire click event on file input when click on the preview image.
+
+        The test flow is:
+            - Navigate to Widget Add Page.
+            - Assert if Preview is not present.
+            - Choice a image.
+            - Click on the preview image.
+            - Assert if click event is fired on input file.
+        """
+        self.goto_add_page()
+
+        form_row = self.find_widget_form_row()
+        self.assertIsNone(self.find_widget_preview(form_row))
+        
+        file_input = form_row.query_selector('input[type=file]')
+        file_input.set_input_files(self.image2)
+
+        self.inject_input_file_clicked()
+
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNotNone(preview)
+        img = preview.query_selector('img')
+        img.click()
+        self.assert_input_file_clicked()
+        
+    def test_should_open_preview_modal_when_click_on_preview_button(self):
+        """
+        Should open the preview modal when click on the preview button.
+
+        The test flow is:
+            - Navigate to Widget Add Page.
+            - Assert if preview is not present.
+            - Choice a image.
+            - Assert if preview is present.
+            - Click on the preview button on the preview image.
+            - Assert if the preview modal is visible.
+        """
+        self.goto_add_page()
+
+        form_row = self.find_widget_form_row()
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNone(preview)
+        file_input = form_row.query_selector('input[type=file]')
+        file_input.set_input_files(self.image2)
+        
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNotNone(preview)
+        preview_img = preview.query_selector('img')
+
+        preview_button = self.find_preview_icon(preview)
+        preview_button.click()
+        
+        self.assert_preview_modal(preview_img)
+
+    def test_should_not_close_preview_modal_when_click_image_inside_it(self):
+        """
+        Should not close the preview modal when click on the image inside it.
+
+        The test flow is:
+            - Navigate to Widget Add Page.
+            - Assert if preview is not present.
+            - Choice a image.
+            - Assert if preview is present.
+            - Click on the preview button on the preview image.
+            - Assert if the preview modal is visible.
+            - Click on the image inside preview modal.
+            - Wait for 0.5 ms.
+            - Assert if the preview modal is visible.
+        """
+        self.goto_add_page()
+
+        form_row = self.find_widget_form_row()
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNone(preview)
+        file_input = form_row.query_selector('input[type=file]')
+        file_input.set_input_files(self.image1)
+        
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNotNone(preview)
+        preview_img = preview.query_selector('img')
+
+        preview_button = self.find_preview_icon(preview)
+        preview_button.click()
+
+        self.assert_preview_modal(preview_img)
+        preview_modal = self.get_preview_modal(True, 3000)
+
+        img = preview_modal.query_selector('img')
+        img.click()
+
+        self.wait(0.5)
+
+        self.assertEqual(preview_modal.get_attribute("class"), "iuw-modal visible")
+        
+    def test_should_close_preview_modal_when_click_on_close_button_inside_it(self):
+        """
+        Should close the preview modal when click on close button inside it.
+
+        The test flow is:
+            - Navigate to Widget Add Page.
+            - Assert if preview is not present.
+            - Choice a image.
+            - Assert if preview is present.
+            - Click on the preview button on the preview image.
+            - Assert if the preview modal is visible.
+            - Click on the image inside preview modal.
+            - Click on the close button.
+            - Assert if the preview modal is not visible.
+        """
+        self.goto_add_page()
+
+        form_row = self.find_widget_form_row()
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNone(preview)
+        file_input = form_row.query_selector('input[type=file]')
+        file_input.set_input_files(self.image1)
+        
+        preview = self.find_widget_preview(form_row)
+        self.assertIsNotNone(preview)
+        preview_img = preview.query_selector('img')
+
+        preview_button = self.find_preview_icon(preview)
+        preview_button.click()
+        
+        self.assert_preview_modal(preview_img)
+        self.assert_preview_modal_close()
