6e0a0a76c3c7271029ccbf0ccc8507d8350c242e
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-02-26 23:15:39 -0300

    feat(tests): begin creating test model for ArrayField

--- tests/settings.py
+++ tests/settings.py
@@ -70,11 +70,11 @@ if os.environ.get("DATABASE_USE_POSTGRES", "0") == "1":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "NAME": os.environ.get("POSTGRES_DATABASE"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "PORT": os.environ.get("POSTGRES_PORT"),
+            "HOST": os.environ.get("POSTGRES_HOST", 'localhost'),
+            "NAME": os.environ.get("POSTGRES_DATABASE", 'postgres'),
+            "USER": os.environ.get("POSTGRES_USER", 'postgres'),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'postgres'),
+            "PORT": os.environ.get("POSTGRES_PORT", '5432'),
         }
     }
 else:
