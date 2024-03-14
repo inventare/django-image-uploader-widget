648cb0b6db1d4ee922d2963041a3d0be9dba3326
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-03-14 10:56:43 -0300

    feat: temp work

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import re_path, path, reverse
+from django.shortcuts import redirect
 
 urlpatterns = (
     [
         re_path('admin/', admin.site.urls),
+        path('', lambda _: redirect(reverse('admin:index'))),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
