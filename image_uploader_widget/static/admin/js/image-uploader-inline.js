1dfa2559fcdb2c0a92f700be75485d96c20553c0
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-03-18 18:56:01 -0300

    fix: small fixes on Safari

--- image_uploader_widget/static/admin/js/image-uploader-inline.js
+++ image_uploader_widget/static/admin/js/image-uploader-inline.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let DRAGGING_EDITOR = null;
+    let DRAGGING_COUNTER = 0;
     window.uploaderEditors = {};
     const DELETE_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="100%" height="100%"><path xmlns="http://www.w3.org/2000/svg" d="m289.94 256 95-95A24 24 0 0 0 351 127l-95 95-95-95a24 24 0 0 0-34 34l95 95-95 95a24 24 0 1 0 34 34l95-95 95 95a24 24 0 0 0 34-34z"></path></svg>';
     const PREVIEW_ICON = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="100%" height="100%"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"></path><path xmlns="http://www.w3.org/2000/svg" d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"></path><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"></path></svg>';
@@ -270,6 +271,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function handleDragEnter(e) {
+        DRAGGING_COUNTER = DRAGGING_COUNTER + 1;
         const editor = getEditor(e.target);
         if (!editor) {
             return;
@@ -290,6 +292,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     function handleDragLeave(e) {
+        DRAGGING_COUNTER = DRAGGING_COUNTER - 1;
+        if (DRAGGING_COUNTER > 0) {
+            return;
+        }
+
         if (!DRAGGING_EDITOR) {
             return;
         }
@@ -374,7 +381,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function handleDrop(e) {
         e.preventDefault();
-        
+
         const editor = DRAGGING_EDITOR;
         if (!editor) {
             return;
@@ -386,7 +393,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const draggingElement = document.getElementById(editor.element.getAttribute('dragging-element'));
             editor.element.removeAttribute('dragging-element');
             const x = e.pageX;
-            const y = e.pageY;
+            const y = e.pageY - window.scrollY;
             const newIndex = getElementNewPosition(editor, draggingElement, x, y);
             reorderItems(editor, draggingElement, newIndex);
             return;
