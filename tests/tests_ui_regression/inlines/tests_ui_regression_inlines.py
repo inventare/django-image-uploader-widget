d0e55a89b05ff38f144eb59e7ccd9ca18670bde7
Eduardo Oliveira <eduardo_y05@outlook.com>
2024-08-21 09:24:12 -0300

    fix(tests): change testing mode for add button

--- tests/tests_ui_regression/inlines/tests_ui_regression_inlines.py
+++ tests/tests_ui_regression/inlines/tests_ui_regression_inlines.py
@@ -112,8 +112,18 @@ class InlineEditorUIRegressionTestCase(test_case.IUWTestCase):
         self.goto_change_page()
         self.page.set_viewport_size({"width": 800, "height": 800})
 
-        root = self.find_inline_root()
-        self.assert_match_snapshot(root, "in_test_add_button_on_small_screen")
+        self.wait(0.5)
+
+        has_scrollbars = self.page.evaluate(
+            """() => {
+            var element = document.querySelector(".iuw-inline-root");
+            return element.scrollWidth > element.clientWidth;
+        }"""
+        )
+        self.assertTrue(has_scrollbars)
+
+        add_button = self.page.query_selector(".iuw-add-image-btn")
+        self.assert_match_snapshot(add_button, "in_test_add_button_on_small_screen")
 
     def test_ui_initialized_toggle_dark_theme(self):
         major, minor, _, _, _ = django.VERSION
