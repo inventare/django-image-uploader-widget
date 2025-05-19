c576a47adfde7316888c58f92c4bcdf0b112394e
Eduardo Oliveira <eduardo_y05@outlook.com>
2025-05-18 22:00:02 -0300

    chore: remove ui regression tests

--- tests/utils/test_case.py
+++ tests/utils/test_case.py
@@ -1,34 +1,26 @@
 import os
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from match_snapshot import MatchSnapshot
 from playwright.sync_api import sync_playwright
 
 from tests.pom.django_admin import DjangoAdminPO
 
 
-class TestCase(MatchSnapshot, StaticLiveServerTestCase):
+class TestCase(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         super().setUpClass()
         cls.playwright = sync_playwright().start()
         cls.browser = cls.playwright.chromium.launch()
-        cls.context = cls.browser.new_context(
-            viewport={
-                "width": 1280,
-                "height": 720
-            }
-        )
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
-        cls.context.close()
         cls.browser.close()
         cls.playwright.stop()
 
     def setUp(self):
-        self.page = self.context.new_page()
+        self.page = self.browser.new_page()
         self.admin_po = DjangoAdminPO(self.page, self.live_server_url)
         self.admin_po.create_user_and_execute_login()
