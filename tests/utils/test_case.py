12df34cd3c750d442f11035784dd0ddad65bb449
Eduardo Oliveira <eduardo_y05@outlook.com>
2025-05-18 21:47:17 -0300

    chore: test

--- tests/utils/test_case.py
+++ tests/utils/test_case.py
@@ -14,14 +14,21 @@ class TestCase(MatchSnapshot, StaticLiveServerTestCase):
         super().setUpClass()
         cls.playwright = sync_playwright().start()
         cls.browser = cls.playwright.chromium.launch()
+        cls.context = cls.browser.new_context(
+            viewport={
+                "width": 1280,
+                "height": 720
+            }
+        )
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
+        cls.context.close()
         cls.browser.close()
         cls.playwright.stop()
 
     def setUp(self):
-        self.page = self.browser.new_page()
+        self.page = self.context.new_page()
         self.admin_po = DjangoAdminPO(self.page, self.live_server_url)
         self.admin_po.create_user_and_execute_login()
