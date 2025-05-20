276b3a63df709160f4db9bf974c6cc354889b2a1
Eduardo Oliveira <eduardo_y05@outlook.com>
2025-05-20 15:10:06 -0700

    feat: add max files property to inline and array field

--- demo/array_field/models.py
+++ demo/array_field/models.py
@@ -4,7 +4,7 @@ from image_uploader_widget.postgres.fields import ImageListField
 
 
 class TestWithArrayField(models.Model):
-    images = ImageListField(blank=True, null=True, upload_to="admin_test")
+    images = ImageListField(blank=True, null=True, max_images=2, upload_to="admin_test")
 
     class Meta:
         verbose_name = "(Array Field) Default"
